fa32fa386cb26264e230bf14e9151857541d3280
Caprichoo <sandeepkb3899@outlook.com>
2024-02-29 00:40:04 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("**Welcome to Temperature Convertor**\n")
+option = int(input('Do you want to convert to celsius  from fahrenheit or vice-versa, Type 1 for celsius  to fahrenheit or Type 2 for fahrenheit to celsius \n'))
+if option == 1:
+    celsius = float(input('Enter the temperature in celsius : '))
+    result = float((celsius * 9/5) + 32)
+    print(f'The temperature in fahrenheit is {result}')
+elif option == 2:
+    fahrenheit = float(input('Enter the temperature in fahrenheit: '))
+    result1 = float((fahrenheit - 32) * 5/9)
+    print(f'The temperature in celsius is {result1}')
